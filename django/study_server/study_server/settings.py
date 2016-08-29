791b61eb833566213d512da3c5f30fb0704e210e
jhon.buendia <jhon.buendia@globant.com>
2016-08-29 15:27:26 -0500

    smtp configuration

--- django/study_server/study_server/settings.py
+++ django/study_server/study_server/settings.py
@@ -116,8 +116,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-EMAIL_HOST = 'smtp.book2study.com'
+EMAIL_HOST = 'dime200.dizinc.com'
 EMAIL_HOST_USER = 'mail@book2study.com'
 EMAIL_HOST_PASSWORD = '6674889'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
